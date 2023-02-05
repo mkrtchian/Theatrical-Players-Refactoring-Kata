7d8c78634b2249f07b4da0110ada02151f522b39
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-05 14:08:07 +0100

    replace temp with query for play variable

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -28,14 +28,14 @@ function statement(invoice: Invoice, plays: Plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
@@ -43,6 +43,10 @@ function statement(invoice: Invoice, plays: Plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function playFor(aPerformance: Performance) {
+    return plays[aPerformance.playID];
+  }
 
   function amountFor(aPerformance: Performance, play: Play) {
     let thisAmount = 0;
