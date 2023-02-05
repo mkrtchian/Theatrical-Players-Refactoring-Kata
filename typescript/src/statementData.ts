dfb851263b9b1a5a9213353083c185391274a379
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-05 22:46:25 +0100

    move amount computation to subclasses

--- typescript/src/statementData.ts
+++ typescript/src/statementData.ts
@@ -54,31 +54,9 @@ export function createStatementData(invoice: Invoice, plays: Plays) {
 
 class PerformanceCalculator {
   constructor(
-    private readonly aPerformance: Performance,
-    private readonly play: Play
+    protected readonly aPerformance: Performance,
+    protected readonly play: Play
   ) {}
-
-  get amount() {
-    let thisAmount = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (this.aPerformance.audience > 30) {
-          thisAmount += 1000 * (this.aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (this.aPerformance.audience > 20) {
-          thisAmount += 10000 + 500 * (this.aPerformance.audience - 20);
-        }
-        thisAmount += 300 * this.aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return thisAmount;
-  }
 
   get volumeCredits() {
     let result = 0;
@@ -89,6 +67,23 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let thisAmount = 40000;
+    if (this.aPerformance.audience > 30) {
+      thisAmount += 1000 * (this.aPerformance.audience - 30);
+    }
+    return thisAmount;
+  }
+}
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let thisAmount = 30000;
+    if (this.aPerformance.audience > 20) {
+      thisAmount += 10000 + 500 * (this.aPerformance.audience - 20);
+    }
+    thisAmount += 300 * this.aPerformance.audience;
+    return thisAmount;
+  }
+}
