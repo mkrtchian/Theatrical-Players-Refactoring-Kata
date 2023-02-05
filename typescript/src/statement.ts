29d7dafdb8a1c3c430ce70f17f2cdf248d94daa6
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-05 14:45:12 +0100

    rename variable inside totalVolumeCredits

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -42,11 +42,11 @@ function statement(invoice: Invoice, plays: Plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 
   function volumeCreditsFor(aPerformance: Performance) {
