7886a5bbb822535405afc66513de767b5f85c7e9
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-05 22:06:47 +0100

    move statementData and types to 2 other files

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -1,34 +1,5 @@
-type Play = {
-  name: string;
-  type: string;
-};
-
-type Plays = {
-  [key: string]: Play;
-};
-
-type Performance = {
-  playID: string;
-  audience: number;
-};
-
-type Invoice = {
-  customer: string;
-  performances: Performance[];
-};
-
-type EnrichedPerformance = Performance & {
-  play: Play;
-  amount: number;
-  volumeCredits: number;
-};
-
-type StatementData = {
-  customer: string;
-  performances: EnrichedPerformance[];
-  totalAmount: number;
-  totalVolumeCredits: number;
-};
+import { createStatementData } from "./statementData";
+import { Invoice, Plays, StatementData } from "./types";
 
 function statement(invoice: Invoice, plays: Plays) {
   return renderPlaintext(createStatementData(invoice, plays));
@@ -53,70 +24,6 @@ function renderPlaintext(statementData: StatementData) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber);
-  }
-}
-
-function createStatementData(invoice: Invoice, plays: Plays) {
-  const enrichedPerformances = invoice.performances.map(enrichPerformance);
-  const statementData = {
-    customer: invoice.customer,
-    performances: enrichedPerformances,
-    totalAmount: totalAmount(enrichedPerformances),
-    totalVolumeCredits: totalVolumeCredits(enrichedPerformances),
-  };
-  return statementData;
-
-  function enrichPerformance(aPerformance: Performance) {
-    const play = playFor(aPerformance);
-    const result = {
-      ...aPerformance,
-      play,
-      amount: amountFor({ ...aPerformance, play }),
-      volumeCredits: volumeCreditsFor({ ...aPerformance, play }),
-    };
-    return result;
-  }
-
-  function playFor(aPerformance: Performance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance: Performance & { play: Play }) {
-    let thisAmount = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (aPerformance.audience > 30) {
-          thisAmount += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (aPerformance.audience > 20) {
-          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        thisAmount += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return thisAmount;
-  }
-
-  function volumeCreditsFor(aPerformance: Performance & { play: Play }) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function totalAmount(performances: EnrichedPerformance[]) {
-    return performances.reduce((total, perf) => total + perf.amount, 0);
-  }
-
-  function totalVolumeCredits(performances: EnrichedPerformance[]) {
-    return performances.reduce((total, perf) => total + perf.volumeCredits, 0);
   }
 }
 
