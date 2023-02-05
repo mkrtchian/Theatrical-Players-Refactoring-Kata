17642f4e3c883a684733baed2783e41d7404b303
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-05 14:22:53 +0100

    rename format function

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -25,16 +25,16 @@ function statement(invoice: Invoice, plays: Plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function format(aNumber: number) {
+  function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
