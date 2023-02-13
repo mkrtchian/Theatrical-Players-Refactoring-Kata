3e37263d6851c8328bdda831a82acaeacdd6b4ca
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-13 14:51:05 +0100

    remove ccomment

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -23,9 +23,7 @@ function statement(invoice: Invoice, plays: Plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // add volume credits
     volumeCredits += volumeCreditFor(perf);
-    // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
