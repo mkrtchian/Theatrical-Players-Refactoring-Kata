5e06abff01947a5d24a86f20debae09885d4b417
Roman Mkrtchian <mkrtchia@gmail.com>
2023-02-16 11:19:07 +0100

    inline total mount and rename

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -26,9 +26,7 @@ function statement(invoice: Invoice, plays: Plays) {
     } seats)\n`;
   }
 
-  const totalAmount = nomBidon();
-
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(getTotalAmount() / 100)}\n`;
   result += `You earned ${totalVolumeCredit()} credits\n`;
   return result;
 
@@ -82,7 +80,7 @@ function statement(invoice: Invoice, plays: Plays) {
     return result;
   }
 
-  function nomBidon() {
+  function getTotalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);
